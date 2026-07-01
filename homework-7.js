3765a8895adce00efc89fb5a1ad5bec8bda91b7b
RustamSharkasi <rustam.alsharkasi@gmail.com>
2026-07-01 08:52:00 +0300

    homework 07

---
+++ homework-7.js
@@ -0,0 +1,32 @@
+const showTempSity = (sity, temp) => {
+  console.log(`Сейчас в ${sity} температура — ${temp} градусов по Цельсию`)
+}
+
+showTempSity('Medina', '38')
+
+const LIGHT_SPEED = 299
+
+function checkSpeed(speed) {
+  if (speed > LIGHT_SPEED) {
+    console.log('Сверхсветовая скорость')
+  } else if (speed === LIGHT_SPEED) {
+    console.log('Скорость света')
+  } else {
+    console.log('Субсветовая скорость')
+  }
+}
+
+checkSpeed(300)
+
+let product = 'Хлеб'
+const price = 50
+
+function checkBudget(budget) {
+  if (budget >= price) {
+    console.log(`${product} приобретён. Спасибо за покупку!`)
+  } else {
+    console.log(`Вам не хватает ${price - budget}, пополните баланс`)
+  }
+}
+
+checkBudget(40)
