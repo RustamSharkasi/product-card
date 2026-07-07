f351773fd260ff949a7498ddfba60b5ae9d100bc
RustamSharkasi <rustam.alsharkasi@gmail.com>
2026-07-08 00:20:44 +0300

    homework 08 correction 1

--- homework-8.js
+++ homework-8.js
@@ -16,7 +16,9 @@ const person = {
 const myAuto = {
   brand: 'Hyundai',
   model: 'Sonata',
+
   year: 2004,
+
   color: 'White',
   transmission: 'Automatic',
   maxspeed: 240
@@ -48,23 +50,77 @@ showPropertyValue(myAuto, 'model');
 // Задание 7. Создаем массив книг
 
 const russianClassics = [
-  { title: "Война и мир", author: "Лев Толстой", year: 1869, coverColor: "Светло-серый", genre: "Роман" },
-  { title: "Евгений Онегин", author: "Александр Пушкин", year: 1833, coverColor: "Голубой ", genre: "Роман" },
-  { title: "Анна Каренина", author: "Лев Толстой", year: 1878, coverColor: "Коричневый", genre: "Роман" },
-  { title: "Герой нашего времени", author: "Михаил Лермонтов", year: 1840, coverColor: "Бежевый", genre: "Роман" }
+  {
+    title: "Война и мир",
+    author: "Лев Толстой",
+    year: 1869,
+    coverColor: "Светло-серый",
+    genre: "Роман"
+  },
+  {
+    title: "Евгений Онегин",
+    author: "Александр Пушкин",
+    year: 1833,
+    coverColor: "Голубой ",
+    genre: "Роман"
+  },
+  {
+    title: "Анна Каренина",
+    author: "Лев Толстой",
+    year: 1878,
+    coverColor: "Коричневый",
+    genre: "Роман"
+  },
+  {
+    title: "Герой нашего времени",
+    author: "Михаил Лермонтов",
+    year: 1840,
+    coverColor: "Бежевый",
+    genre: "Роман"
+  }
 ];
 
-russianClassics.push({ title: "Преступление и наказание", author: "Федор Достоевский", year: "1866", coverColor: "Тёмно-синий", genre: "Роман" });
+russianClassics.push({
+  title: "Преступление и наказание",
+  author: "Федор Достоевский",
+  year: "1866"
+  , coverColor: "Тёмно-синий",
+  genre: "Роман"
+});
 
 console.log(russianClassics);
 
 // Задание 8. ОбЬединяем массивы в один
 
 const englishClassics = [
-  { title: "Гордость и предубеждение", author: "Джейн Остин", year: 1813, coverColor: "Кремовый", genre: "Роман" },
-  { title: "Джейн Эйр", author: "Шарлотта Бронте", year: 1847, coverColor: "Тёмно-синий", genre: "Драма" },
-  { title: "Портрет Дориана Грея", author: "Оскар Уайльд", year: 1890, coverColor: "Чёрный", genre: "Роман" },
-  { title: "Приключения Оливера Твиста", author: "Чарльз Диккенс", year: 1838, coverColor: "Коричневый", genre: "Роман" }
+  {
+    title: "Гордость и предубеждение",
+    author: "Джейн Остин",
+    year: 1813,
+    coverColor: "Кремовый",
+    genre: "Роман"
+  },
+  {
+    title: "Джейн Эйр",
+    author: "Шарлотта Бронте",
+    year: 1847,
+    coverColor: "Тёмно-синий",
+    genre: "Драма"
+  },
+  {
+    title: "Портрет Дориана Грея",
+    author: "Оскар Уайльд",
+    year: 1890,
+    coverColor: "Чёрный",
+    genre: "Роман"
+  },
+  {
+    title: "Приключения Оливера Твиста",
+    author: "Чарльз Диккенс",
+    year: 1838,
+    coverColor: "Коричневый",
+    genre: "Роман"
+  }
 ];
 
 const uniteBooks = [...russianClassics, ...englishClassics];
@@ -76,7 +132,9 @@ console.log(uniteBooks);
 let chekRareBooks = uniteBooks.map(function (book) {
   return {
     ...book,
-    isRare: book.year < 1850
+    isRare: book.
+      year < 1850
+
   };
 });
 
